6b2184d990ff674e061ceb021ef3e7c48e919035
bikash <bikashghimirey797@gmail.com>
2017-05-10 16:29:20 +0545

    class 3rd

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
-	echo "hello word" ; //single line comment
+	/*echo "hello word" ; */ //single line comment
 	/* multi line comment */ 
+
+	$string_1 = "hello \"biru voli game\" 'cha'";
+	$string_2 = 'hello \'bikash voli xam\' cha';
+
+	$html = "<p>this is  paragraph</p>";
+	echo $html;
+	const PI=3.14;
+	define('G',9.8);
+	echo PI; 
+	echo "<br/>";
+	echo G;
 
 ?>
 
