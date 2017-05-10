87bbaa41b7ed16c87e7e00310fb4208ecd4b5b93
gituser <sandesh.bhattarai79@gmail.com>
2017-05-10 15:31:36 +0545

    Git pushed for next

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+	/*Data types in PHP*/
+	echo "String Data types";
+	echo "<hr />"; // horizontal line
+
+	$string = "This is string variable";
+	$string_1 = 'This is string variable';
+	echo $string;
+	
+	echo "<br/>"; //Line Break
+	echo $string_1;
+	echo "<br/>";
+
+	echo "$string"; // This is string variable
+
+	echo '$string'; // $string
+	echo "<br/>";
+
+	echo "Number Data types";
+	echo "<hr />";
+
+	$num = 123;
+	$num_1 =12.3;
+	echo $num;
+	echo "<br />";
+	echo $num_1;
+	echo "<br />";
+
+	$string = 123.45;
+	echo $string;
+	echo "<br />";
+
+	echo "Array Data types";
+	echo "<hr />";
+
+	$array = array( 1, 2, 3, 4, 5, 6);
+	echo $array[4];
+
+
+?>
