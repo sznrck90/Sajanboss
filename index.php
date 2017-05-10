1cd90f0a87cf1412ccfd561ac332e7a4b4845d3d
Bibu Subedi <nephop.dx69@gmail.com>
2017-05-10 15:49:47 +0545

    bibu push

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+echo 'hello world'; //single line comment
+       /*multi
+       line
+       comment*/
+        // ""
+       // ''  
+        echo "<br />";
+       $string = "string 1";
+       echo "string";
+       echo 'string';
+        echo "<br />";
+       
+
+
+
+       $str='string';
+       echo $str;
+       $str="value";
+       echo $str;
+
+       $num=123456;
+       echo $num;
+       echo "<br />";
+
+       $float=123.123;
+
+
+
+
+       echo "Array";
+       echo"<hr />";
+
+       $array_var = array(1,'string',3,array('a','b'));
+       echo $array_var[2];
+
+ ?>
