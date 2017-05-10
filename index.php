c780c084f849f05ec7e6e140bcc1616c8ae1a347
Bibu Subedi <nephop.dx69@gmail.com>
2017-05-10 16:31:06 +0545

    php in html

--- index.php
+++ index.php
@@ -34,4 +34,19 @@ echo 'hello world'; //single line comment
        $array_var = array(1,'string',3,array('a','b'));
        echo $array_var[2];
 
+
+       $html1 = "<p align = 'center'> This is paragraph tag. </p>";
+       echo $html1;
+       $html = "<img src = '' />";
+       echo $html; 
+      
+         /*php constant*/
+         define('PI',3.14);
+
+         const PI_1 = 3.14;
+         echo PI;
+
+         echo PI_1;
+
+
  ?>
