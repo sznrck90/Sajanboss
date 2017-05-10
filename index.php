00c7b86939b6e25121e81416a4df85f1638322d9
Sandesh Bhattarai <sandesh.bhattarai79@gmail.com>
2017-05-10 16:20:24 +0545

    HTML within PHP and vice versa

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	/*Data types in PHP*/
-	echo "String Data types";
+	/*echo "String Data types";
 	echo "<hr />"; // horizontal line
 
 	$string = "This is string variable";
@@ -34,7 +34,40 @@
 	echo "<hr />";
 
 	$array = array( 1, 2, 3, 4, 5, 6);
-	echo $array[4];
+	echo $array[4];*/
 
 
+$string_1 = "Lorem \"ipsum dolor sit amet, consectetur adipisicing elit\". Nam porro maxime 'repudiandae reprehenderit' autem mollitia adipisci assumenda, sapiente similique. Amet quam dicta rerum earum ad. Voluptatibus praesentium voluptas vitae at!";
+
+$string_2 ='Lorem \'ipsum dolor sit amet\', consectetur adipisicing elit. Fugit facere nesciunt ducimus culpa repellat et sunt! Quas aliquam, maxime repudiandae doloribus reprehenderit? Culpa explicabo nemo possimus, pariatur. Dolores, nobis, fugiat.';
+	
+	$html1 = "<p align='center'>This is paragraph Tag.</p>";
+	$html = "<img width='100%' src='assets/images/chauka.jpg' />";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>This is Index page</title>
+</head>
+<body>
+	<div>
+		<?php echo $html1; ?>
+	</div>
+	<div>
+		<?php
+			echo $html;
+			/*PHP Constants*/
+			define('PI',3.14);
+
+			/*define("PI",3.14);*/
+
+			const PI_1 = 3.14;
+			echo PI;
+
+			echo PI_1;
+		?>
+	</div>
+</body>
+</html>
